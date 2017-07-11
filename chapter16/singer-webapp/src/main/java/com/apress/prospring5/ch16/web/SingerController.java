ceb2d4efd8066e0473c1db834ed027f853196217
Iuliana Cosmina <iuliana.cosmina@gmail.com>
2017-07-12 00:33:19 +0300

    93 added functional application with singers

---
+++ chapter16/singer-webapp/src/main/java/com/apress/prospring5/ch16/web/SingerController.java
@@ -0,0 +1,204 @@
+package com.apress.prospring5.ch16.web;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Locale;
+
+import com.apress.prospring5.ch16.util.Message;
+import com.apress.prospring5.ch16.util.SingerGrid;
+import com.apress.prospring5.ch16.util.UrlUtil;
+import com.apress.prospring5.ch16.entitites.Singer;
+import com.apress.prospring5.ch16.services.SingerService;
+import com.google.common.collect.Lists;
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
+import javax.validation.Valid;
+
+@RequestMapping("/singers")
+@Controller
+public class SingerController {
+    private final Logger logger = LoggerFactory.getLogger(SingerController.class);
+
+    private SingerService singerService;
+    private MessageSource messageSource;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String list(Model uiModel) {
+        logger.info("Listing singers");
+
+        List<Singer> singers = singerService.findAll();
+        uiModel.addAttribute("singers", singers);
+
+        logger.info("No. of singers: " + singers.size());
+
+        return "singers/list";
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public String show(@PathVariable("id") Long id, Model uiModel) {
+        Singer singer = singerService.findById(id);
+        uiModel.addAttribute("singer", singer);
+
+        return "singers/show";
+    }
+
+    @RequestMapping(value = "/{id}", params = "form", method = RequestMethod.POST)
+    public String update(@Valid Singer singer, BindingResult bindingResult, Model uiModel,
+                         HttpServletRequest httpServletRequest, RedirectAttributes redirectAttributes,
+                         Locale locale) {
+        logger.info("Updating singer");
+        if (bindingResult.hasErrors()) {
+            uiModel.addAttribute("message", new Message("error",
+                    messageSource.getMessage("singer_save_fail", new Object[]{}, locale)));
+            uiModel.addAttribute("singer", singer);
+            return "singers/update";
+        }
+        uiModel.asMap().clear();
+        redirectAttributes.addFlashAttribute("message", new Message("success",
+                messageSource.getMessage("singer_save_success", new Object[]{}, locale)));
+        singerService.save(singer);
+        return "redirect:/singers/" + UrlUtil.encodeUrlPathSegment(singer.getId().toString(),
+                httpServletRequest);
+    }
+
+    @RequestMapping(value = "/{id}", params = "form", method = RequestMethod.GET)
+    public String updateForm(@PathVariable("id") Long id, Model uiModel) {
+        uiModel.addAttribute("singer", singerService.findById(id));
+        return "singers/update";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String create(@Valid Singer singer, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest, RedirectAttributes redirectAttributes, Locale locale, @RequestParam(value="file", required=false) Part file) {
+        logger.info("Creating singer");
+        if (bindingResult.hasErrors()) {
+            uiModel.addAttribute("message", new Message("error",
+                    messageSource.getMessage("singer_save_fail", new Object[]{}, locale)));
+            uiModel.addAttribute("singer", singer);
+            return "singers/create";
+        }
+        uiModel.asMap().clear();
+        redirectAttributes.addFlashAttribute("message", new Message("success",
+                messageSource.getMessage("singer_save_success", new Object[]{}, locale)));
+
+        logger.info("Singer id: " + singer.getId());
+
+        // Process upload file
+        if (file != null) {
+            logger.info("File name: " + file.getName());
+            logger.info("File size: " + file.getSize());
+            logger.info("File content type: " + file.getContentType());
+            byte[] fileContent = null;
+            try {
+                InputStream inputStream = file.getInputStream();
+                if (inputStream == null) logger.info("File inputstream is null");
+                fileContent = IOUtils.toByteArray(inputStream);
+                singer.setPhoto(fileContent);
+            } catch (IOException ex) {
+                logger.error("Error saving uploaded file");
+            }
+            singer.setPhoto(fileContent);
+        }
+
+        singerService.save(singer);
+        return "redirect:/singers/";
+    }
+
+    @RequestMapping(value = "/photo/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public byte[] downloadPhoto(@PathVariable("id") Long id) {
+        Singer singer = singerService.findById(id);
+
+        if (singer.getPhoto() != null) {
+            logger.info("Downloading photo for id: {} with size: {}", singer.getId(),
+                    singer.getPhoto().length);
+        }
+
+        return singer.getPhoto();
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @RequestMapping(params = "form", method = RequestMethod.GET)
+    public String createForm(Model uiModel) {
+        Singer singer = new Singer();
+        uiModel.addAttribute("singer", singer);
+
+        return "singers/create";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/listgrid", method = RequestMethod.GET, produces="application/json")
+    public SingerGrid listGrid(@RequestParam(value = "page", required = false) Integer page,
+                                @RequestParam(value = "rows", required = false) Integer rows,
+                                @RequestParam(value = "sidx", required = false) String sortBy,
+                                @RequestParam(value = "sord", required = false) String order) {
+
+        logger.info("Listing singers for grid with page: {}, rows: {}", page, rows);
+        logger.info("Listing singers for grid with sort: {}, order: {}", sortBy, order);
+
+        // Process order by
+        Sort sort = null;
+        String orderBy = sortBy;
+        if (orderBy != null && orderBy.equals("birthDateString"))
+            orderBy = "birthDate";
+
+        if (orderBy != null && order != null) {
+            if (order.equals("desc")) {
+                sort = new Sort(Sort.Direction.DESC, orderBy);
+            } else
+                sort = new Sort(Sort.Direction.ASC, orderBy);
+        }
+
+        // Constructs page request for current page
+        // Note: page number for Spring Data JPA starts with 0, while jqGrid starts with 1
+        PageRequest pageRequest = null;
+
+        if (sort != null) {
+            pageRequest = new PageRequest(page - 1, rows, sort);
+        } else {
+            pageRequest = new PageRequest(page - 1, rows);
+        }
+
+        Page<Singer> singerPage = singerService.findAllByPage(pageRequest);
+
+        // Construct the grid data that will return as JSON data
+        SingerGrid singerGrid = new SingerGrid();
+
+        singerGrid.setCurrentPage(singerPage.getNumber() + 1);
+        singerGrid.setTotalPages(singerPage.getTotalPages());
+        singerGrid.setTotalRecords(singerPage.getTotalElements());
+
+        singerGrid.setSingerData(Lists.newArrayList(singerPage.iterator()));
+
+        return singerGrid;
+    }
+
+    @Autowired
+    public void setSingerService(SingerService singerService) {
+        this.singerService = singerService;
+    }
+
+    @Autowired
+    public void setMessageSource(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+}
---
+++ chapter16/singer-webapp/src/main/resources/sql/test-data.sql
@@ -0,0 +1,14 @@
+insert into singer (first_name, last_name, birth_date) values ('John', 'Mayer', '1977-10-16');
+insert into singer (first_name, last_name, birth_date) values ('Eric', 'Clapton', '1945-03-30');
+insert into singer (first_name, last_name, birth_date) values ('John', 'Butler', '1975-04-01');
+insert into singer (first_name, last_name, birth_date) values ('B.B.', 'King', '1925-09-16');
+insert into singer (first_name, last_name, birth_date) values ('Jimi', 'Hendrix', '1942-11-27');
+insert into singer (first_name, last_name, birth_date) values ('Jimmy', 'Page', '1944-01-09');
+insert into singer (first_name, last_name, birth_date) values ('Eddie', 'Van Halen', '1955-01-26');
+insert into singer (first_name, last_name, birth_date) values ('Saul (Slash)', 'Hudson', '1965-07-23');
+insert into singer (first_name, last_name, birth_date) values ('Stevie', 'Ray Vaughan', '1954-10-03');
+insert into singer (first_name, last_name, birth_date) values ('David', 'Gilmour', '1946-03-06');
+insert into singer (first_name, last_name, birth_date) values ('Kirk', 'Hammett', '1992-11-18');
+insert into singer (first_name, last_name, birth_date) values ('Angus', 'Young', '1955-03-31');
+insert into singer (first_name, last_name, birth_date) values ('Dimebag', 'Darrell', '1966-08-20');
+insert into singer (first_name, last_name, birth_date) values ('Carlos', 'Santana', '1947-07-20');
