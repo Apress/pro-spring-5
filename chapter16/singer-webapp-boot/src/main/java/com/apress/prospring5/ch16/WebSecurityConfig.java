36853b5c1338d17d81153e700f7f2f23acc21bd6
Iuliana Cosmina <iuliana.cosmina@gmail.com>
2018-02-21 23:27:55 +0200

    Corrected behaviour of singer-webapp-boot byt adding {noop} in front of the password to avoid adding a Password Encoder

--- chapter16/singer-webapp-boot/src/main/java/com/apress/prospring5/ch16/WebSecurityConfig.java
+++ chapter16/singer-webapp-boot/src/main/java/com/apress/prospring5/ch16/WebSecurityConfig.java
@@ -30,6 +30,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
-                .withUser("user").password("user").roles("USER");
+                .withUser("user").password("{noop}user").roles("USER");
     }
 }
