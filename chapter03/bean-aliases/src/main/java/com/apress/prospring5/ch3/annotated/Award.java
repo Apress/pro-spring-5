a859ee82a066f7b7f38c30697bc9339067790d28
iuliana <iuliana.cosmina@gmail.com>
2019-06-03 22:36:50 +0100

    Fix for issue 14

--- chapter03/bean-aliases/src/main/java/com/apress/prospring5/ch3/annotated/Award.java
+++ chapter03/bean-aliases/src/main/java/com/apress/prospring5/ch3/annotated/Award.java
@@ -2,9 +2,14 @@ package com.apress.prospring5.ch3.annotated;
 
 import org.springframework.core.annotation.AliasFor;
 
+import java.lang.annotation.*;
+
 /**
  * Created by iuliana.cosmina on 2/19/17.
  */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface Award {
 
 	@AliasFor("prize")
