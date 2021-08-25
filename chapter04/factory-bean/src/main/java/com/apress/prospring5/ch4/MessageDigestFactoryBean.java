b2a06f65d74c2ac603132619675e44f51f22f6db
Sergey Mazurchuk <sermaz@gmail.com>
2021-08-25 20:42:54 +0300

    Changed deprecate method

--- chapter04/factory-bean/src/main/java/com/apress/prospring5/ch4/MessageDigestFactoryBean.java
+++ chapter04/factory-bean/src/main/java/com/apress/prospring5/ch4/MessageDigestFactoryBean.java
@@ -5,8 +5,7 @@ import java.security.MessageDigest;
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class MessageDigestFactoryBean implements 
-      FactoryBean<MessageDigest>, InitializingBean {
+public class MessageDigestFactoryBean implements FactoryBean<MessageDigest>, InitializingBean {
     private String algorithmName = "MD5";
 
     private MessageDigest messageDigest = null;
