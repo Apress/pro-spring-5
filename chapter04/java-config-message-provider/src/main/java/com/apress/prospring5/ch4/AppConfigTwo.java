37de6297a995371a47cf32aca6bfaf6df4601e22
Sergey Mazurchuk <sermaz@gmail.com>
2021-08-25 21:25:06 +0300

    DI implemented using constructor

--- chapter04/java-config-message-provider/src/main/java/com/apress/prospring5/ch4/AppConfigTwo.java
+++ chapter04/java-config-message-provider/src/main/java/com/apress/prospring5/ch4/AppConfigTwo.java
@@ -3,15 +3,19 @@ package com.apress.prospring5.ch4;
 import com.apress.prospring5.ch2.decoupled.MessageProvider;
 import com.apress.prospring5.ch2.decoupled.MessageRenderer;
 import com.apress.prospring5.ch2.decoupled.StandardOutMessageRenderer;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages={"com.apress.prospring5.ch4.annotated"})
 public class AppConfigTwo {
 
-	@Autowired
-	MessageProvider provider;
+	final MessageProvider provider;
+
+	public AppConfigTwo(MessageProvider provider) {
+		this.provider = provider;
+	}
 
 
 	@Bean(name = "messageRenderer")
