14a0fa0d94d7087716875ca79998a8ca57bb8cf4
iuliana <iuliana.cosmina@gmail.com>
2022-02-10 01:07:30 +0000

    Removed useless files

--- chapter06/plain-jdbc/src/main/resources/db/ddl.sql
+++
@@ -1,8 +0,0 @@
-CREATE USER 'prospring5'@'localhost' IDENTIFIED BY 'prospring5';
-
-CREATE SCHEMA MUSICDB;
-GRANT ALL PRIVILEGES ON MUSICDB . * TO 'prospring5'@'localhost';
-FLUSH PRIVILEGES;
-
-/*in case of java.sql.SQLException: The server timezone value 'UTC' is unrecognized or represents more than one timezone. */
-SET GLOBAL time_zone = '+3:00';
