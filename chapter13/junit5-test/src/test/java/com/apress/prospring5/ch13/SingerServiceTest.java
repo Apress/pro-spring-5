b73215e29a1e1bae28bf9f23cb5bc3598a2c9c97
iuliana <iuliana.cosmina@gmail.com>
2022-05-27 17:03:03 +0200

    Added javax.annottation-api dependency and fixed the build

--- chapter13/junit5-test/src/test/java/com/apress/prospring5/ch13/SingerServiceTest.java
+++ chapter13/junit5-test/src/test/java/com/apress/prospring5/ch13/SingerServiceTest.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
+
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.jdbc.SqlGroup;
